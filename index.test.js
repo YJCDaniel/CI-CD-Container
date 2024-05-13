c222deef41904b005891c4f9d22e6e0b341c6db3
YJCDaniel <yjcdaniel@hotmail.com>
2024-05-13 20:50:06 +0800

    edit ci.yaml and cd.yaml

--- index.test.js
+++ index.test.js
@@ -1,29 +1,10 @@
-const { pranata, pranatapost } = require('./index')
+const request = require('supertest');
+const app = require('./index');
 
-describe('pranata', () => {
-  it('should return status code 200 and the correct message', async () => {
-    const event = {};
-    const expectedResponse = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Go Serverless v3.0! Get function executed successfully!",
-      }, null, 2),
-    };
-    const result = await pranata(event);
-    expect(result).toEqual(expectedResponse);
-  });
-});
-
-describe('pranatapost', () => {
-  it('should return status code 200 and the correct message', async () => {
-    const event = {};
-    const expectedResponse = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Go Serverless v3.0! Post function executed successfully!",
-      }, null, 2),
-    };
-    const result = await pranatapost(event);
-    expect(result).toEqual(expectedResponse);
+describe('GET /', () => {
+  it('responds with "Hello, world!"', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello, world!');
   });
 });
